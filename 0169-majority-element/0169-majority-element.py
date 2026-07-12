bbcb222eccd1ef933431587d4aa4ae1713554ae8
Ashwin <ashwinchinta7@gmail.com>
2026-07-12 18:10:58 +0530

    Time: 6 ms (61.25%), Space: 21.1 MB (85.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hash = {}
-        for i in range(0,len(nums)):
-            if nums[i] not in hash:
-                hash[nums[i]] = 1
-            else:
-                hash[nums[i]]+=1
-        max_val = 0
-        for key,val in hash.items():
-            if val > max_val:
-                max_val = val
+        can = None
+        cnt = 0
 
-        for key,val in hash.items():
-            if val == max_val:
-                return key
-    
+        for num in nums:
+            if cnt == 0:
+                can = num
+            if num == can:
+                cnt+=1
+            else:
+                cnt-=1
+        return can
         
