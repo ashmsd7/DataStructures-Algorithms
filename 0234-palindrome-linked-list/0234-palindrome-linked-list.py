72a752509ac53bb353b3b31887c744cd3a8ace11
Ashwin <ashwinchinta7@gmail.com>
2026-07-24 00:15:23 +0530

    Time: 40 ms (36.24%), Space: 42.5 MB (63.95%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,14 +3,33 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        p1 = head
-        stacker = []
-        while p1:
-            stacker.append(p1.val)
-            p1 = p1.next
-        return stacker == stacker[::-1]
+        slow = head
+        fast = head
 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        
+        prev = None
+        curr = slow
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        front = head
+        back = prev
+
+        while back:
+            if front.val != back.val:
+                return False
+
+            front = front.next
+            back = back.next
+
+        return True
