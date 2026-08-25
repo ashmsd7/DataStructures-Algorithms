e01c261ed2ceaa0feba2457017c2c315f34673b6
Ashwin <ashwinchinta7@gmail.com>
2026-08-25 11:00:15 +0530

    Time: 139 ms (41.5%), Space: 31.7 MB (45.42%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        dp= [0] *n
+        dp[0] = nums[0]
+        dq = deque([0])
+
+        for i in range(1,n):
+            while dq and dq[0] < i - k:
+                dq.popleft()
+            
+            dp[i] = dp[dq[0]] + nums[i]
+
+            while dq and dp[dq[-1]] < dp[i]:
+                dq.pop()
+            
+            dq.append(i)
+        
+        return dp[dq[-1]]
+
+
+
+        
