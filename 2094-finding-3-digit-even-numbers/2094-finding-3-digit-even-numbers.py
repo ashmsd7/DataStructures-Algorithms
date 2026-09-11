1194c69d7d5f73a19df40613681c444e4d5eaa52
Ashwin <ashwinchinta7@gmail.com>
2026-09-11 16:01:39 +0530

    Time: 5963 ms (5.13%), Space: 21.3 MB (6.67%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        res = set()
+
+        def backtrack(path,seen_indices):
+            if len(path) ==3:
+                if path[-1]%2 == 0 and path[0]!=0:
+                    number = 100 * path[0] + 10* path[1] + path[2]
+                    res.add(number)
+                return 
+
+            for index in range(len(digits)):
+                if index in seen_indices:
+                    continue
+
+                if not path and digits[index]== 0:
+                    continue
+                
+                seen_indices.add(index)
+                path.append(digits[index])
+
+                backtrack(path,seen_indices)
+
+                path.pop()
+                seen_indices.remove(index)
+            
+        backtrack([],set())
+        return sorted(list(res))
